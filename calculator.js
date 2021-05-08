3f1ac45c0a59e9365017f30fd8e99a5a6c5463a5
Sonali Gupta <sonalitraintech@gmail.com>
2021-05-08 14:20:20 +0530

    BMI calculator Added.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+// To parse the behind process on server to get the output
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Calculator
 app.get("/", function(req, res){
 res.sendFile(__dirname +"/index.html");
 });
@@ -11,6 +15,19 @@ app.post("/", function(req, res){
   var result = num1 + num2;
   res.send("The result is : " +result);
 });
+
+// BMICalculator
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname +"/bmiCalculator.html");
+});
+app.post("/bmicalculator", function(req, res){
+  var personHeight =  Number(req.body.height);
+  var personWeight =  Number(req.body.weight);
+  var personBMI = Math.round(personWeight / (personHeight * personHeight));
+  res.send("Your BMI score is : " +personBMI);
+});
+
+// Server listen function
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
 });
